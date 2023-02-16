d8226ac048cef8b46db88ae94eaacf6b56f7cdda
antalmate12 <30597152+antalmate12@users.noreply.github.com>
2023-02-16 17:50:10 +0100

    arrow fn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component, Fragment } from 'react';
-import {PDFViewer} from '@react-pdf/renderer'
-import Invoice from './components/reports/Invoice'
-import invoice from './data/invoice'
+import React, {Fragment} from "react";
+import {PDFViewer, PDFDownloadLink} from "@react-pdf/renderer";
+import Invoice from "./components/reports/Invoice";
+import invoice from "./data/invoice";
 
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
         <Fragment>
-            <PDFViewer width="1000" height="600" className="app" >
+            <PDFViewer width="1000" height="600" className="app">
                 <Invoice invoice={invoice}/>
             </PDFViewer>
+
+            <PDFDownloadLink document={<Invoice invoice={invoice}/>} fileName="somename.pdf">
+                {({blob, url, loading, error}) => {
+                    {loading ? "Loading document..." : "Download now!"}</span>
+
+
+                    
+                }}
+            </PDFDownloadLink>
         </Fragment>
     );
-  }
+
 }
 
 export default App;
